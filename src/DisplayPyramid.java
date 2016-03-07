d344a5fbb6993efbc7d2968840b2f31bc7fe5c11
nataliecr <pikayoshi23@gmail.com>
2016-03-07 14:12:03 -0800

    Update DisplayPyramid.java
    previously, pyramid had leaned to the left

--- src/DisplayPyramid.java
+++ src/DisplayPyramid.java
@@ -5,14 +5,13 @@ public class DisplayPyramid {
     Scanner input = new Scanner(System.in);
     System.out.print("Enter the number of lines: ");
     int num_lines = input.nextInt();
-    int count = 1;
     
     if (num_lines > 15 || num_lines < 1)
       System.exit(0);
     
     for (int i = 1; i <= num_lines; i++){
       for (int j = 1; j <= (num_lines - i); j++)
-        System.out.print(" ");
+        System.out.print("  ");
       
       for (int j = i; j >= 1; j--)
         System.out.print(j + " ");
